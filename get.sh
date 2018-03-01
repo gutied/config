88888ab314ede305690b97d502026ddc7f4ed63c
davidg <davidg@snuffles.com>
2018-03-01 13:51:10 +0000

    Add get script

---
+++ get.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cp ~/.bash_profile ./bash/bash_profile 
+cp ~/.bash_prompt ./bash/bash_prompt 
+cp ~/.bashrc ./bash/bashrc 
+cp ~/.git-completition.bash ./git/git-completition.bash 
+cp ~/.git-prompt.sh ./git/git-prompt.sh 
+cp ~/.tmux.conf ./tmux/tmux.conf 
+cp ~/.vimrc ./vim/vimrc 
+
--- set.sh
+++ set.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 cp ./bash/bash_profile ~/.bash_profile
-cp ./bash/bash_prompt ~/.bash_promt
+cp ./bash/bash_prompt ~/.bash_prompt
 cp ./bash/bashrc ~/.bashrc
+cp ./git/git-completition.bash ~/.git-completition.bash
+cp ./git/git-prompt.sh ~/.git-prompt.sh
 cp ./tmux/tmux.conf ~/.tmux.conf
 cp ./vim/vimrc ~/.vimrc
 
